5807e8efcddb3412dd4c91e0c973e20d5319f3d6
Andrew Mueller <amueller@caltech.edu>
2025-02-13 10:53:38 -0800

    setup

--- setup.sh
+++ setup.sh
@@ -22,8 +22,8 @@ uv sync
 # Print colored instructions
 GREEN='\033[0;32m'
 NC='\033[0m' # No Color
-echo -e "${GREEN}Setup complete! You can now:${NC}"
+echo -e "${GREEN}Setup complete! Do 'cd switch_control' and the either: ${NC}"
 echo -e "${GREEN}1. Run the script with: sh run.sh${NC}"
-echo -e "${GREEN}2. Run directly with UV: cd backend && uv run main.py${NC}"
+echo -e "${GREEN}2. Run directly with uv: cd ./backend/backend && uv run main.py${NC}"
 
 
